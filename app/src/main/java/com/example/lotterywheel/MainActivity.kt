17364b3f16cabed7d82c5bfde7202f44ff7ef2b4
Toolspeter <hua01jan@outlook.com>
2024-10-15 00:33:19 +0800

    maybe added wheel

--- app/src/main/java/com/example/lotterywheel/MainActivity.kt
+++ app/src/main/java/com/example/lotterywheel/MainActivity.kt
@@ -5,16 +5,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.lotterywheel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding.wheelview.draw_wheel(14)
     }
 }
--- app/src/main/java/com/example/lotterywheel/canvas/wheel.kt
+++ app/src/main/java/com/example/lotterywheel/canvas/wheel.kt
@@ -4,35 +4,87 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.RectF
+import android.util.AttributeSet
 import android.view.View
 import androidx.core.content.contentValuesOf
 import androidx.core.graphics.red
+import java.util.jar.Attributes
 
-class wheel(context: Context):View(context) {
-    private val red = Paint().apply {
+class wheel(context: Context,attrs:AttributeSet):View(context,attrs) {
+    private lateinit var rectF:RectF
+    private var counts:Int = 1
+    private var startangle : Float = 0f
+    private var whellangle : Float = 90f
+
+    val red = Paint().apply {
         color = Color.parseColor("#FF0000")
+        strokeWidth = 20f
+        style = Paint.Style.FILL
+        strokeCap = Paint.Cap.ROUND
     }
-    private val orange = Paint().apply {
+    val orange = Paint().apply {
         color = Color.parseColor("#FF7F00")
+                strokeWidth = 20f
+        style = Paint.Style.FILL
+        strokeCap = Paint.Cap.ROUND
     }
-    private val yellow = Paint().apply {
+    val yellow = Paint().apply {
         color = Color.parseColor("#FFFF00")
+                strokeWidth = 20f
+        style = Paint.Style.FILL
+        strokeCap = Paint.Cap.ROUND
     }
-    private val green = Paint().apply {
+    val green = Paint().apply {
         color = Color.parseColor("#00FF00")
+                strokeWidth = 20f
+        style = Paint.Style.FILL
+        strokeCap = Paint.Cap.ROUND
     }
-    private val blue = Paint().apply {
+    val blue = Paint().apply {
         color = Color.parseColor("#00FFFF")
+                strokeWidth = 20f
+        style = Paint.Style.FILL
+        strokeCap = Paint.Cap.ROUND
     }
-    private val indigo = Paint().apply {
+    val indigo = Paint().apply {
         color = Color.parseColor("#0000FF")
+                strokeWidth = 20f
+        style = Paint.Style.FILL
+        strokeCap = Paint.Cap.ROUND
     }
-    private val purple = Paint().apply {
+    val purple = Paint().apply {
         color = Color.parseColor("#8B00FF")
+                strokeWidth = 20f
+        style = Paint.Style.FILL
+        strokeCap = Paint.Cap.ROUND
     }
+    private var paints : Paint = red
 
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        rectF = RectF(w/8f,w/8f,w*7/8f,w*7/8f)
+    }
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        canvas.drawCircle(100f,100f,50f,red)
+        for(i in 0 until counts){
+            startangle = i.toFloat()/counts*360f
+            whellangle = 360f/counts*1f
+            val temp = i%7
+            when(temp){
+                0 -> paints = red
+                1 -> paints = orange
+                2 -> paints = yellow
+                3 -> paints = green
+                4 -> paints = blue
+                5 -> paints = indigo
+                6 -> paints = purple
+            }
+            canvas.drawArc(rectF,startangle,whellangle,true,paints)
+        }
+    }
+    fun draw_wheel(count:Int){
+        counts = count
+        invalidate()
     }
 }
